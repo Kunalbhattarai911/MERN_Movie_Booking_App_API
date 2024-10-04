ad0b55afa233bf2798346469df4e5b31a6d80b91
Kunalbhattarai911 <Kunalbhattarai911@gmail.com>
2024-10-04 22:37:30 +0545

    1

--- backend/src/databaseConnection/connectDB.js
+++ backend/src/databaseConnection/connectDB.js
@@ -1,15 +1,23 @@
 import mongoose from "mongoose";
 
+let isConnected; // track the connection status
+
 const dbConnection = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Successfully Connected To Database');
+    isConnected = true;
+    console.log("Successfully connected to the database");
   } catch (error) {
-    console.error("Error Details:", error);
-    throw new Error("Error while connecting to database: " + error.message);
+    isConnected = false;
+    console.error("Database connection error:", error);
   }
 };
 
